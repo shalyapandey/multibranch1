6a2a722e3c672fffcd7e449c4af2683183811e8c
root <root@ip-172-31-45-31.ap-south-1.compute.internal>
2019-08-10 10:52:40 +0000

    adding jenkins installation script

---
+++ installjenkins.sh
@@ -0,0 +1,3 @@
+udo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+sudo rpm --import http://pkg.jenkins-ci.org/redhat-stable/jenkins-ci.org.key
+sudo yum install jenkins
